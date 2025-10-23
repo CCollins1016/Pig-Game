b0c65e5d08ed241b440b0e5fcf74cfcba8d72bfa
CCollins1016 <ccollins@bsd220.org>
2025-10-23 10:26:31 -0500

    Added Pig Images

--- Pig Game/ContentView.swift
+++ Pig Game/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Hello, world!")
+        ZStack{
+            Color.gray.opacity(0.7).ignoresSafeArea()
+            VStack {
+                Image("pig").resizable().frame(width: 150, height: 150)
+                Text("Pig")
+                Spacer()
+            }
         }
     }
 }
