3e8b71c21f475f1864ea327691634828a0a18e7c
CCollins1016 <ccollins@bsd220.org>
2025-10-23 10:17:47 -0500

    Initial Commit

---
+++ Pig Game/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Pig Game
+//
+//  Created by Christian Collins on 10/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pig Game/Pig_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pig_GameApp.swift
+//  Pig Game
+//
+//  Created by Christian Collins on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Pig_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
